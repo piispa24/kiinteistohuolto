32ed87e4f5aa232c37c566910d3c170a0f68dc6b
Joonas Piispanen <piispa24@gmail..com>
2023-04-26 17:09:01 +0300

    uudet App välilehdet

---
+++ tyontekijaApp.php
@@ -0,0 +1,31 @@
+<?php require "header.php"; ?>
+
+<div class="container-fluid p-5">
+  <div class="row">
+    <div class="col-6 p-5 tyontekijaRivi">
+      <a class="underline" href="###">
+        <button class="tyontekijaBtn">Katso vikailmoitukset</button>
+      </a>
+    </div>
+    <div class="col-6 p-5 tyontekijaRivi">
+      <a class="underline" href="###">
+        <button class="tyontekijaBtn">Katso omat työtehtävät</button>
+      </a>
+    </div>
+  </div>
+  <div class="row tyontekijaRivi">
+    <div class="col-6 p-5 tyontekijaRivi">
+      <a class="underline" href="###">
+        <button class="tyontekijaBtn">Siirrä ilmoitus omalle työlistalle</button>
+      </a>
+    </div>
+    <div class="col-6 p-5 tyontekijaRivi"> 
+      <a class="underline" href="###">
+        <button class="tyontekijaBtn">Kuittaa työ tehdyksi</button>
+      </a>
+    </div>
+  </div>
+</div>
+
+
+<?php require "footer.php"; ?>
---
+++ vikailmoitusApp.php
@@ -0,0 +1,12 @@
+<?php require "header.php"; ?>
+
+<div id="vikailmoitusBtn" class="container-fluid">
+    <div class="col-">
+        <a href="###">
+            <button id="vikailmoitusBtn">Tee vikailmoitus</button>
+        </a>
+
+    </div>
+</div>
+
+<?php require "footer.php"; ?>
