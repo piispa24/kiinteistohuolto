76f4e2932bd18990f329e769db7e24e0afde1f49
william <william.lonnberg@gmail.com>
2023-04-28 16:46:15 +0300

    ulkoasua

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php require "header.php"; ?>
 
-<h1>Etusivu</h1>
-<h1>Etusivu</h1>
-<h1>Etusivu</h1>
-<h1>Etusivu</h1>
-<h1>Etusivu</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-6 p-5">
+            <h2>Tervetuloa!</h2>
+            <p>Tervetuloa kiinteistöhuolto Piispanen & Lönnberg sivustolle. Sivustoltamme löydät
+                yhteystietomme, yhteydenottolomakkeen ja mahdollisuuden jättää vikailmoituksia.
+
+            </p>
+        </div>
+        <div class="col-sm-6">
+            <img src="img/condo-2414328_960_720-removebg-preview.png" alt="Description of image" class="img-fluid cont-image">
+        </div>
+    </div>
+</div>
 
 <?php require "footer.php"; ?>
