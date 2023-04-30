2090303969aa9ca9c469a7eed07b78e26f77a6aa
william <william.lonnberg@gmail.com>
2023-04-30 12:18:56 +0300

    indexin ulkoasua

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php require "header.php"; ?>
 
 <div class="container-fluid container-index vh-100">
-    <div class="row">
-      <div class="col-sm-6 p-5 text-white">
-        <h3 class="display-6 font-weight-bold" style="font-weight: 400;">Kiinteistöhuolto <br>Piispanen & Lönnberg Oy</h3>
-        <h1 class="display-3 font-weight-bold" style="font-weight: 900;">Kiinteistöpalvelua <br>kaikenkokoisille <br>taloyhtiöille Helsingissä</h1>
-      </div>
-      <div class="col-sm-6 p-0">
-        <!--<img src="img/condo-2414328_960_720-removebg-preview(1).png" alt="Description of image" class="img-fluid cont-image">-->
-      </div>
+  <div class="row pt-5">
+    <div class="col-sm-6 p-5 indextext">
+      <h3 class="display-6 font-weight-bold indexheader" style="font-weight: 400;">Kiinteistöhuolto <br>Piispanen & Lönnberg Oy</h3>
+      <h1 class="display-3 font-weight-bold indexheader" style="font-weight: 900;">Kiinteistöpalvelua <br>kaiken kokoisille <br>taloyhtiöille Helsingissä</h1>
     </div>
-
-    <div class="row">
-      <div class="col-sm-6 p-5 text-white">
-        <h4 style="color:red">24h PÄIVYSTYS - 0502223344</h3>
-        <p><a class="underline btn btn-grad2" href="contact.php">Ota yhteyttä</a></p>
-      </div>
-      <div class="col-sm-6 p-0">
-      </div>
+    <div class="col-sm-6 p-0">
+      <!--<img src="img/condo-2414328_960_720-removebg-preview(1).png" alt="Description of image" class="img-fluid cont-image">-->
     </div>
   </div>
+
+  <div class="row">
+    <div class="col-sm-6 p-5 text-white indextext">
+      <h4>24h PÄIVYSTYS - 0502223344</h3>
+      <p><a class="underline btn btn-grad2" href="contact.php">Ota yhteyttä</a></p>
+    </div>
+    <div class="col-sm-6 p-0">
+    </div>
+  </div>
+</div>
+
 
 <?php require "footer.php"; ?>
