0f756d9b3c7b65eaeb8c185b75f2d7c03c0d95bf
Joonas Piispanen <piispa24@gmail..com>
2023-05-02 17:00:45 +0300

    kk

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ session_start();
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">    
     <link rel="stylesheet" href="styles/style.css">
-    <link rel="icon" type="image/png" href="img/company.png">
+    <link rel="icon" type="image/png" href="img/testi11.png">
 
     <!-- Javascript -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -22,7 +22,7 @@ session_start();
   </head>
   <body>
     <div class=" bg-danger">
-      <div class="ylarivi">Päivystys 24h 050 2223 344 • piispanenlonnberg @ kiinteistohuolto.com</div>
+      <div class="ylarivi">Päivystys 24h 050 2223 344 • paivystys @ kiinteistohuolto.com</div>
     </div>
     <nav class="sticky-top navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
--- yhteystiedot.php
+++ yhteystiedot.php
@@ -2,11 +2,11 @@
 
 
 <div class="container-fluid container-index vh-100 p-5">
-    <div class="container p-5 pyoristys">
+    
 
         <div class="row mb-4">
             <div class="col-sm-12 muutext">
-            <h3>Yhteystiedot</h3>
+            
             </div>
         </div>
 
@@ -14,20 +14,20 @@
 
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Päivystys 24h</h5>
-                <p>0501112223</p>
+                <h5 class="valkTekst">Päivystys 24h</h5>
+                <p class="valkTekst">0501112223</p>
             </div>
 
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Asiakaspalvelu</h5>
-                <p>0502223344</p>
+                <h5 class="valkTekst">Asiakaspalvelu</h5>
+                <p class="valkTekst">0502223344</p>
             </div>
             
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Toimisto avoinna</h5>
-                <p>Ma-pe klo 8:00-15:00<br>La klo 8:00-11.00</p>
+                <h5 class="valkTekst">Toimisto avoinna</h5>
+                <p class="valkTekst">Ma-pe klo 8:00-15:00<br>La klo 8:00-11.00</p>
                 <p></p>
             </div>
 
@@ -37,25 +37,25 @@
             
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Toimisto</h5>
-                <p>Piispanen & Lönnberg Oy<br>Malmitie 15<br>00001 Helsinki</p>
+                <h5 class="valkTekst">Toimisto</h5>
+                <p class="valkTekst">Piispanen & Lönnberg Oy<br>Malmitie 15<br>00001 Helsinki</p>
             </div>
 
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Hallinto</h5>
-                <p><strong>William Lönnberg</strong> - 0503434344<br><strong>Joonas Piispanen</strong> - 04023232323</p>
+                <h5 class="valkTekst">Hallinto</h5>
+                <p class="valkTekst"><strong>William Lönnberg</strong> - 0503434344<br><strong>Joonas Piispanen</strong> - 04023232323</p>
             </div>
             
             <div class="col-sm-4 text-center muutext">
                 <div class="gradient-circle"></div><br>
-                <h5>Sähköposti</h5>
-                <p>piispanenlonnberg@kiinteistohuolto.com</p>
+                <h5 class="valkTekst">Sähköposti</h5>
+                <p class="valkTekst">piispanenlonnberg@kiinteistohuolto.com</p>
             </div>
 
         </div>
 
-    </div>
+   
 </div>
 
 <?php require "footer.php"; ?>
