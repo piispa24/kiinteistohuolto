002c9b928f31e5ff8013bcc44465bfdce9322a39
Joonas Piispanen <piispa24@gmail..com>
2023-05-05 12:14:11 +0300

    f

--- paivitaTyo.php
+++ paivitaTyo.php
@@ -4,9 +4,10 @@ require "connect.php";
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
 
 if(!isset($_SESSION['email'])){
-    header("Location: tyokirjautuminen.php");
+    header("Location: kirjautuminen.php");
     exit;
 }
 
